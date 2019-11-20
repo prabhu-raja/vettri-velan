38ebb5c19615a1de5e930b3cf60be926010c3139
Prabhu <prabhu.raja@rci.rogers.com>
2019-11-20 07:25:29 -0500

    Rx - 2 - Cleanup the observables manually 🧻

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,24 +14,36 @@ export class AppComponent implements OnInit {
   }
 
   section1() {
+    const observer = {
+      next: (val: string) => console.log('next - 🚀', val),
+      error: (err: any) => console.log('error - 🚨', err),
+      complete: () => console.log('complete! - 👍')
+    };
+
     const observable = new Observable(subscriber => {
-      subscriber.next('Hello');
-      subscriber.next('World');
-      subscriber.complete();
+      let count = 0;
+      const myInterval = setInterval(() => {
+        subscriber.next(count);
+        count++;
+        if (count > 4) {
+          subscriber.complete();
+        }
+      }, 1000);
+
+      return () => {
+        console.log('called and clearing interval');
+        clearInterval(myInterval);
+      };
     });
 
-    // const observer = {
-    //   next: (val: string) => console.log('next - 🚀', val),
-    //   error: (err: any) => console.log('error - 🚨', err),
-    //   complete: () => console.log('complete! - 👍')
-    // };
+    console.log('🏁');
+    observable.subscribe(observer);
+    console.log('✋');
 
-    // observable.subscribe(observer);
-
-    observable.subscribe(
-      test => console.log(`Iam with ${test}`),
-      err => console.error(`Im an Error ${err}`),
-      () => console.log('finally Iam completed')
-    );
+    // observable.subscribe(
+    //   test => console.log(`Iam with ${test}`),
+    //   err => console.error(`Im an Error ${err}`),
+    //   () => console.log('finally Iam completed')
+    // );
   }
 }
