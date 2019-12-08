f1466e1b15d326814e312a698c120e9955bfcf8a
prabhu-raja <prabhuraja@gmail.com>
2019-12-08 08:14:22 -0500

    Routing - Custom Preloading Strategies 🚥

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes, PreloadingStrategy, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ScrollBarComponent } from './scroll-bar/scroll-bar.component';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { Observable, of } from 'rxjs';
 // import { SampleModule } from './sample/sample.module';
+
+export class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data && route.data.isPreload ? fn() : of(null);
+  }
+}
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'scroll', component: ScrollBarComponent },
-  { path: 'sample', loadChildren: './sample/sample.module#SampleModule'},
+  {
+    path: 'sample',
+    data: {
+      isPreload: true
+    },
+    loadChildren: './sample/sample.module#SampleModule'
+  },
   { path: '**', component: NotfoundComponent},
 ];
 
@@ -26,10 +39,11 @@ const routes: Routes = [
     BrowserModule,
     // SampleModule,
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules // * This will preload all lazy load modules
+      // preloadingStrategy: PreloadAllModules // * This will preload all lazy load modules
+      preloadingStrategy: CustomPreload // * This is custom preload
     })
   ],
-  providers: [],
+  providers: [CustomPreload],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
