01394fe938cc248d796bd699b31737b96077bccb
prabhu-raja <prabhuraja@gmail.com>
2019-12-24 06:22:56 -0500

    Rx- 21 - debounceTime

--- src/app/sample/rx-rate-limiting/rx-rate-limiting.component.ts
+++ src/app/sample/rx-rate-limiting/rx-rate-limiting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, pluck, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rx-rate-limiting',
@@ -10,6 +12,22 @@ export class RxRateLimitingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.practiseDebounce();
+  }
+
+  practiseDebounce() {
+    // elements
+    const inputBox = document.getElementById('text-input');
+    // streams
+    const input$ = fromEvent(inputBox, 'keyup');
+    input$
+      .pipe(
+        debounceTime(1000),
+        pluck('target', 'value'),
+        distinctUntilChanged()
+      )
+      .subscribe(console.log);
+
   }
 
 }
