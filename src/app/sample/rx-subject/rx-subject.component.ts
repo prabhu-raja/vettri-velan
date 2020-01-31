6438554121b0727173ea9630c55e20525115331f
prabhu-raja <prabhuraja@gmail.com>
2020-01-30 19:09:40 -0500

    Rx - 43 - Subject vs BehaviorSubject

--- src/app/sample/rx-subject/rx-subject.component.ts
+++ src/app/sample/rx-subject/rx-subject.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject, interval, Observable } from 'rxjs';
-import { tap, share, take, multicast, refCount } from 'rxjs/operators';
+import { Subject, interval, Observable, BehaviorSubject } from 'rxjs';
+import { tap, share, take, multicast, refCount, withLatestFrom } from 'rxjs/operators';
 import { MulticastOperator } from 'rxjs/internal/operators/multicast';
 
 @Component({
@@ -15,7 +15,9 @@ export class RxSubjectComponent implements OnInit {
   ngOnInit() {
     // this.kickStart();
     // this.beforeMulticast();
-    this.afterMulticast();
+    // this.afterMulticast();
+    // this.normalSubject();
+    this.normalBehaviorSubject();
   }
 
   kickStart() {
@@ -98,6 +100,54 @@ export class RxSubjectComponent implements OnInit {
       subOne.unsubscribe();
       subTwo.unsubscribe();
     }, 4000);
+  }
+
+  normalSubject() {
+    const observer = {
+      next: val => console.log('next: ', val),
+      error: err => console.log('error: ', err),
+      complete: () => console.log('complete')
+    };
+    const subject = new Subject();
+    //
+    const oneSub = subject.subscribe(val => console.log('oneSub', val));
+    subject.next('Hello');
+    const twoSub = subject.subscribe(val => console.log('twoSub', val));
+    subject.next('World');
+    /*
+    Output:
+    oneSub Hello
+    oneSub World
+    twoSub World
+    */
+  }
+
+  /*
+   ! Main Diff b/w Subject and BehaviorSubject
+   ! In BehaviorSubject we can receive last emit value or/and if we want emit initial seed value
+  */
+
+  normalBehaviorSubject() {
+    const bs = new BehaviorSubject('Hi');
+    //
+    bs.next('111');
+    bs.next('222');
+    const oneSub = bs.subscribe(val => console.log('oneBS', val));
+    bs.next('Hello');
+    bs.next('Hello🇨🇦🇨🇦🇨🇦🇨🇦');
+    bs.next('Hello 🇩🇪🇩🇪🇩🇪🇩🇪');
+    const twoSub = bs.subscribe(val => console.log('twoBS', val));
+    bs.next('World');
+    /*
+    Output:
+    oneBS 222
+    oneBS Hello
+    oneBS Hello🇨🇦🇨🇦🇨🇦🇨🇦
+    oneBS Hello 🇩🇪🇩🇪🇩🇪🇩🇪
+    twoBS Hello 🇩🇪🇩🇪🇩🇪🇩🇪
+    oneBS World
+    twoBS World
+    */
   }
 
 }
