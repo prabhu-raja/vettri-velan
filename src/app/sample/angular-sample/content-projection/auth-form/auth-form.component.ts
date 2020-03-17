97bd594b0916a48da3e6387d68a7c9955d60c6ae
prabhu-raja <prabhuraja@gmail.com>
2020-03-17 18:58:15 -0400

    Content Projection - @ContentChild and ngAfterContentInit

--- src/app/sample/angular-sample/content-projection/auth-form/auth-form.component.ts
+++ src/app/sample/angular-sample/content-projection/auth-form/auth-form.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentInit, OnDestroy } from '@angular/core';
+import { AuthRememberComponent } from 'src/app/sample/angular-sample/content-projection/auth-remember/auth-remember.component';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements OnInit, AfterContentInit, OnDestroy {
 
+  @ContentChild(AuthRememberComponent, { static: false}) authRemember: AuthRememberComponent;
+  showMsg: boolean;
+  alive = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    // * After the ng-content initialized we can access the AuthRememberComponent
+    if (this.authRemember) {
+      this.authRemember.checked
+        .pipe(
+          takeWhile(() => this.alive)
+        )
+        .subscribe(val => this.showMsg = val);
+    }
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
   }
 
 }
